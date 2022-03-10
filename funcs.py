3738777ba92e98b8e549ed2c4849179a6bc4e289
Mack Finkel <mackfinkel@gmail.com>
2022-03-10 16:32:45 -0500

    test

--- funcs.py
+++ funcs.py
@@ -2,14 +2,11 @@ import probablepeople as pp
 import usaddress
 import humps
 
-
-# TODO maybe add formatting https://usaddress.readthedocs.io/en/latest/ to create comparable add1 add2
-# TODO consider street name formatting, standardization ("ave","AV","AVE.", "avenue" all saved separate at the moment"
-# TODO consider lookups for common spellings ("philly, phila.")
-
-#test
-
 def address_str_to_dict(string="5757 South Woodlawn avenue, Chicago, IL 60637"):
+    """Convert address string into standardized dictionary"""
+    # TODO consider lookups for common spellings ("philly, phila.")
+    # TODO consider street name formatting, standardization ("ave","AV","AVE.", "avenue" all saved separate at the moment"
+    # TODO maybe add formatting https://usaddress.readthedocs.io/en/latest/ to create comparable add1 add2
     d = dict.fromkeys(usaddress.LABELS)
     try:
         tagged_address, address_type = usaddress.tag(string)
@@ -20,8 +17,8 @@ def address_str_to_dict(string="5757 South Woodlawn avenue, Chicago, IL 60637"):
     d = {**d, **tagged_address}
     return humps.decamelize(d)
 
-
 def name_str_to_dict(string="Davids, David Dave Davie", type='person'):
+    """Convert a person's name string into standardized dictionary"""
     d = dict.fromkeys(pp.LABELS)
     try:
         tagged_name, name_type = pp.tag(string, type)
@@ -36,3 +33,5 @@ def name_str_to_dict(string="Davids, David Dave Davie", type='person'):
 def test_parse():
     print(name_str_to_dict())
     print(address_str_to_dict())
+
+test_parse()
